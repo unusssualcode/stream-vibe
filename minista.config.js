3da62b58f54630080726b3da4fff2893b78454a7
Denis <densmm@ukr.net>
2025-06-13 18:01:34 +0300

    fonts added

--- minista.config.js
+++ minista.config.js
@@ -79,7 +79,12 @@ export default defineConfig({
       localsConvention: "camelCaseOnly",
     },
     preprocessorOptions: {
-      scss: {},
+      scss: {
+        additionalData: `
+        @use '@/styles/helpers' as *;
+        `,
+        silenceDeprecations: ["legacy-js-api"],
+      },
       less: {},
       stylus: {},
     },
