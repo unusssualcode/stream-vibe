40135b38e5341203396815c4e94527361671e877
Denis <densmm@ukr.net>
2025-07-27 15:45:04 +0300

    hero

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,3 +1,5 @@
+import Hero from "@/sections/Hero/index.js"
+
 export const metadata = {
   title: "Home",
 }
@@ -5,7 +7,7 @@ export const metadata = {
 export default function () {
   return (
     <>
-      <h1>Главная</h1>
+      <Hero />
     </>
   )
 }
